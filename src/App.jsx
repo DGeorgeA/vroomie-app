0bb73ae29bde02153be799d20ceebc77aebf43b8
DGeorgeA <33799891+DGeorgeA@users.noreply.github.com>
2025-11-15 10:16:56 +0530

    Update App.jsx
    App.jsx import commands added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
-
+import { maybeRedirectToBase44Login } from './api/base44Client';
+maybeRedirectToBase44Login();
 function App() {
   return (
     <>
